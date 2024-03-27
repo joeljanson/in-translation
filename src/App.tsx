9f5cefd8ae2c32591f49cfb127c3d87078f6429d
Joel Janson <joel.janson.johansen@gmail.com>
2024-03-27 21:48:13 +0100

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+// Import your page components
+import Violin1 from "./pages/Violin1";
+/* import Violin2 from './pages/Violin2';
+import Viola from './pages/Viola';
+import Cello from './pages/Cello';
+import DoubleBass from './pages/DoubleBass'; */
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<div>
+				{/* Navigation Links */}
+				<nav>
+					<Link to="/violin1">Violin 1</Link> |
+					<Link to="/violin2">Violin 2</Link> |<Link to="/viola">Viola</Link> |
+					<Link to="/cello">Cello</Link> |
+					<Link to="/doublebass">Double Bass</Link>
+				</nav>
+
+				{/* Route Configuration */}
+				<Routes>
+					<Route path="/violin1" element={<Violin1 />} />
+					{/* 	<Route path="/violin2" element={<Violin2 />} />
+					<Route path="/viola" element={<Viola />} />
+					<Route path="/cello" element={<Cello />} />
+					<Route path="/doublebass" element={<DoubleBass />} /> */}
+				</Routes>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ src/pages/Violin1.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Violin1: React.FC = () => {
+	return (
+		<div>
+			<h1>Violin 1</h1>
+			{/* Content for Violin 1 page */}
+		</div>
+	);
+};
+
+export default Violin1;
